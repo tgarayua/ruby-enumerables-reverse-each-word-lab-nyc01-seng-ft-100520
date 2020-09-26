28a76da26da3caceb96a5b88c1ebe28647601513
Thomas Garayua <tgarayua@gmail.com>
2020-09-26 17:03:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,9 @@ require "pry"
 def reverse_each_word(string)
     reverse = ""
     index = 0
+    new_array = []
+    
+    new_array = string.split("")
     while index < string.length
        reverse = string[index] + reverse
        index += 1

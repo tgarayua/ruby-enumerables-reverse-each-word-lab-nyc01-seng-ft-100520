dd65bac5d4ce13a607f9b257a45a722cf635849a
Thomas Garayua <tgarayua@gmail.com>
2020-09-26 17:15:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(sentence2)
     reverse = ""
     index = 0
     new_array = []
-    new_array = string.split(" ")
+    new_array = sentence2.split(" ")
     while index < new_array.length
        reverse = new_array[index] + reverse
        index += 1

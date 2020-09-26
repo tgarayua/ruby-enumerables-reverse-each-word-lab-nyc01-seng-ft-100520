218f945da0b98b799638ba7a9d4688a32b979177
Thomas Garayua <tgarayua@gmail.com>
2020-09-26 17:22:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ require "pry"
 def reverse_each_word(sentence2)
     reverse = ""
     index = 0
-    new_array = []
+  
     new_array = sentence2.split(" ")
     while index < new_array.length do |element| 
        reverse = element[index].reverse
